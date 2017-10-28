d4767440a3d9f43e00464d52852c834cff850096
Leonardo Quixadá <lquixada@gmail.com>
2017-10-28 21:57:14 -0200

    roll.config refactored.

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,51 @@
+import path from 'path';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
 
 const production = process.env.NODE_ENV === 'production';
-const config = {};
-
-config.ponyfill = {
-  input: 'browser-polyfill.js',
-  output: {
-    file: 'dist/browser.js',
-    format: 'cjs',
-    strict: false
-  },
-  plugins: [
-    resolve({
-      browser: true
-    }),
-    commonjs(),
-    production && uglify(),
-  ],
-  context: 'this',
-  banner: 'var self = {};',
-  footer: 'module.exports = self;'
-};
-
-config.polyfill = {
-  input: 'browser-polyfill.js',
-  output: [{
-    file: 'dist/browser-polyfill.js',
-    format: 'cjs',
-    strict: false
-  },{
-    file: 'dist/cross-fetch.js',
-    format: 'cjs',
-    sourcemap: true,
-    strict: false
-  }],
-  plugins: [
-    resolve({
-      browser: true
-    }),
-    commonjs(),
-    production && uglify(),
-  ],
-  context: 'this'
-};
 
 module.exports = [
-  config.ponyfill,
-  config.polyfill
+  // Ponyfill (wraps polyfill with banner and footer)
+  {
+    input: path.join(__dirname, 'browser-polyfill.js'),
+    output: {
+      file: path.join(__dirname, 'dist', 'browser.js'),
+      format: 'cjs',
+      strict: false
+    },
+    plugins: [
+      resolve({
+        browser: true
+      }),
+      commonjs(),
+      production && uglify(),
+    ],
+    context: 'this',
+    banner: 'var self = {};',
+    footer: 'module.exports = self;'
+  },
+
+  // Polyfill
+  {
+    input: path.join(__dirname, 'browser-polyfill.js'),
+    output: [{
+      file: path.join(__dirname, 'dist',  'browser-polyfill.js'),
+      format: 'cjs',
+      strict: false
+    },{
+      file: path.join(__dirname, 'dist',  'cross-fetch.js'),
+      format: 'cjs',
+      sourcemap: true,
+      strict: false
+    }],
+    plugins: [
+      resolve({
+        browser: true
+      }),
+      commonjs(),
+      production && uglify(),
+    ],
+    context: 'this'
+  }
 ];
