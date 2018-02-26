4ed0d48f99561dd68fa7b83fbc83a49c198890fd
Leonardo Quixadá <lquixada@gmail.com>
2018-02-26 11:17:28 -0300

    more api specs.

--- test/node/index.js
+++ test/node/index.js
@@ -1,6 +1,7 @@
 const runSuite = require('../fetch.spec');
 
-// Add fetch api to the global scope on node environment
+// Add fetch api to the global scope on node environment. The polyfill also uses
+// the ponyfill version, so we're testing both aproaches in one stroke.
 require('../../src/node-polyfill');
 
 // Run suite with that
