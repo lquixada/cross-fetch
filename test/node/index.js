0270ac29a17e91a4a5eedc6eb70b262264b1f39a
Leonardo Quixadá <lquixada@gmail.com>
2018-02-27 18:40:38 -0300

    test/node/index.js now requires setup.js explicitly.

--- test/node/index.js
+++ test/node/index.js
@@ -1,8 +1,9 @@
-const runSuite = require('../fetch.spec');
+// Prepare environment
+require('./setup');
 
 // Add fetch api to the global scope on node environment. The polyfill also uses
 // the ponyfill version, so we're testing both aproaches in one stroke.
 require('../../src/node-polyfill');
 
 // Run suite with that
-runSuite('node environment');
+require('../fetch.spec')('node environment');
