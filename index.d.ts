3dd9dbe4270ef9a770283318e5f7462377e21c9e
Leonardo Quixadá <lquixada@gmail.com>
2018-05-10 09:40:04 -0300

    added typescript declaration for exported members.

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,12 @@
+declare const fet: typeof fetch;
+declare const req: typeof Headers;
+declare const res: typeof Headers;
+declare const headers: typeof Headers;
+
 declare module "cross-fetch" {
+  export const fetch: typeof fet;
+  export const Request: typeof req;
+  export const Response: typeof res;
+  export const Headers: typeof headers;
   export default fetch;
 }
