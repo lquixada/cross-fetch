24afc74146676163c68bf1c422eeab4a387d0e37
msluther <msluther99@yahoo.com>
2019-01-30 15:42:22 -0800

    [test] Add a test case for the no-op react-native polyfill

--- test/react-native/index.js
+++ test/react-native/index.js
@@ -17,4 +17,23 @@ describe('react-native', function () {
     delete global.Request
     delete global.Response
   })
+
+  it('doesn\'t touch the global functions when polyfilling', function () {
+    const globalFetch = global.fetch = function globalFetch () {}
+    const globalHeaders = global.Headers = function globalHeaders () {}
+    const globalRequest = global.Request = function globalRequest () {}
+    const globalResponse = global.Response = function globalResponse () {}
+
+    require('../../dist/react-native-polyfill')
+
+    expect(fetch).to.equal(globalFetch)
+    expect(Headers).to.equal(globalHeaders)
+    expect(Request).to.equal(globalRequest)
+    expect(Response).to.equal(globalResponse)
+
+    delete global.fetch
+    delete global.Headers
+    delete global.Request
+    delete global.Response
+  })
 })
