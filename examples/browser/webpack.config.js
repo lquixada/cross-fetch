37ea634ff95fd9b751ec0b40ce5204d110caf237
Leonardo Quixadá <lquixada@gmail.com>
2019-07-28 14:04:35 -0400

    simplified examples.

--- examples/browser/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  target: 'web',
-  mode: 'development',
-  entry: './index.js',
-  output: {
-    path: __dirname,
-    filename: 'bundle.js'
-  }
-}
--- examples/node/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  target: 'node',
-  mode: 'development',
-  entry: './index.js',
-  output: {
-    path: __dirname,
-    filename: 'bundle.js'
-  }
-}
