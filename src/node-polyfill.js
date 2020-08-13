5e35b7fc100cc45ed0251ae9ac288c21524b8952
bluelovers <codelovers@users.sourceforge.net>
2020-08-14 05:10:48 +0800

    feat: avoid require when global fetch exists

--- src/node-polyfill.js
+++ src/node-polyfill.js
@@ -1,9 +1,9 @@
-var fetchNode = require('./node-ponyfill')
-var fetch = fetchNode.fetch.bind({})
-
-fetch.polyfill = true
-
 if (!global.fetch) {
+  var fetchNode = require('./node-ponyfill')
+  var fetch = fetchNode.fetch.bind({})
+
+  fetch.polyfill = true
+  
   global.fetch = fetch
   global.Response = fetchNode.Response
   global.Headers = fetchNode.Headers
