9d95ec39b7c10b487ddf6ad7f3dfe70077bf96f6
Leonardo Quixadá <lquixada@gmail.com>
2021-02-04 17:51:34 -0500

    moved test/server to bin/server

--- test/fetch/node/run.sh
+++ test/fetch/node/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 nyc="./node_modules/.bin/nyc"
 mocha="./node_modules/.bin/mocha"
-./test/server $nyc $mocha $(dirname "$0")/index.js
+./bin/server $nyc $mocha $(dirname "$0")/index.js
--- test/fetch/whatwg/run.sh
+++ test/fetch/whatwg/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 browser="./node_modules/.bin/mocha-headless-chrome"
-./test/server $browser -f "$(dirname $0)/index.html?globals=off"
+./bin/server $browser -f "$(dirname $0)/index.html?globals=off"
