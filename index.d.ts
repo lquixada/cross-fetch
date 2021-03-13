7b19cdab1c9b84294032f16a2b13ec258f3a726e
Leonardo Quixadá <lquixada@gmail.com>
2021-03-12 22:18:19 -0500

    cleaned index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -6,22 +6,23 @@ import {
   Response,
 } from "./lib.fetch"
 
+export const fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
 
-declare module "cross-fetch" {
-  export const fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
-  export const Request: {
-    prototype: Request;
-    new(input: RequestInfo, init?: RequestInit): Request;
-  };
-  export const Response: {
-    prototype: Response;
-    new(body?: BodyInit | null, init?: ResponseInit): Response;
-    error(): Response;
-    redirect(url: string, status?: number): Response;
-  };
-  export const Headers: {
-    prototype: Headers;
-    new(init?: HeadersInit): Headers;
-  };
-  export default fetch;
-}
+export const Request: {
+  prototype: Request;
+  new(input: RequestInfo, init?: RequestInit): Request;
+};
+
+export const Response: {
+  prototype: Response;
+  new(body?: BodyInit | null, init?: ResponseInit): Response;
+  error(): Response;
+  redirect(url: string, status?: number): Response;
+};
+
+export const Headers: {
+  prototype: Headers;
+  new(init?: HeadersInit): Headers;
+};
+
+export default fetch;
