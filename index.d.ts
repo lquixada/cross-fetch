e34d41da3c307e0f667b7c539c551f9725811b70
Jonathan Stewmon <jstewmon@gmail.com>
2021-03-22 13:21:48 -0500

    fix: lib.fetch types using experimental ts-graft discover mode
    lib.fetch.d.ts includes all type definitions from the dom and
    dom.iterable libs which were reachable from the original cross-fetch
    index.d.ts file:
    
    ```
    git checkout 7b19cdab1c9b84294032f16a2b13ec258f3a726e -- index.d.ts
    ```
    
    lib.fetch.d.ts was generated with the following command:
    
    ```
    cat <<EOF > .ts-graftrc.yaml \
    && npx ts-graft@2.0.0-1 \
    && rm .ts-graftrc.yaml \
    grafts:
      - source: index.d.ts
        output: lib.fetch.d.ts
        include:
          - dom
          - dom.iterable
    EOF
    ```
    
    index.d.ts was then updated to its new state and verified with the
    following command:
    
    ```
    npx tsc --lib ES6 index.d.ts
    ```
    
    Issue #95 was validated as follows:
    
    ```
    cat <<EOF > test.ts \
    && npx tsc --target ES6 --moduleResolution node --noEmit  test.ts \
    && rm test.ts
    import fetch from "./";
    export const customFetch = (
      input: RequestInfo,
      init: RequestInit,
    ): Promise<Response> => {
      let url = "";
      url += input;
      return fetch(url, init);
    };
    EOF
    ```
    
    n.b. consumers must include the dom.iterable lib, which is implied by
    the ES6 target, lest their globals lack the required members defined
    by dom.iterable.

--- index.d.ts
+++ index.d.ts
@@ -1,33 +1,7 @@
-import type {
-  BodyInit,
-  Headers as BaseHeaders,
-  HeadersInit,
-  Request,
-  RequestInfo,
-  RequestInit,
-  Response,
-  ResponseInit,
-} from "./lib.fetch"
-import type { Headers as IterHeaders } from "./lib.fetch.iterable";
-type Headers = BaseHeaders & IterHeaders;
+import * as libFetch from "./lib.fetch";
 
-export const fetch: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
-
-export const Request: {
-  prototype: Request;
-  new(input: RequestInfo, init?: RequestInit): Request;
-};
-
-export const Response: {
-  prototype: Response;
-  new(body?: BodyInit | null, init?: ResponseInit): Response;
-  error(): Response;
-  redirect(url: string, status?: number): Response;
-};
-
-export const Headers: {
-  prototype: Headers;
-  new(init?: HeadersInit): Headers;
-};
-
+export const fetch: typeof libFetch.fetch;
+export const Request: typeof libFetch.Request;
+export const Response: typeof libFetch.Response;
+export const Headers: typeof libFetch.Headers;
 export default fetch;
