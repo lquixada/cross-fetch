57931dfbe9da6121bf0e493cc4350f6986ed5344
Leonardo Quixadá <lquixada@gmail.com>
2021-10-17 18:06:19 -0400

    feat: added service worker support

--- rollup.config.js
+++ rollup.config.js
@@ -25,7 +25,7 @@ export default [
       name: 'irrelevant',
       strict: false,
       banner: outdent(`
-        // Save the global object in a variable
+        // Save global object in a variable
         var __global__ =
           (typeof globalThis !== 'undefined' && globalThis) ||
           (typeof self !== 'undefined' && self) ||
@@ -53,8 +53,7 @@ export default [
         delete __globalThis__.fetch.polyfill;
 
         // Choose between native implementation (__global__) or custom implementation (__globalThis__)
-        // var ctx = __global__.fetch ? __global__ : __globalThis__;
-        var ctx = __globalThis__ // this line disable service worker support temporarily
+        var ctx = __global__.fetch ? __global__ : __globalThis__;
 
         exports = ctx.fetch // To enable: import fetch from 'cross-fetch'
         exports.default = ctx.fetch // For TypeScript consumers without esModuleInterop.
