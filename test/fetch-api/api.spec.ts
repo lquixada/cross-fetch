926f33fa83a115a8a49f0d8ab8c12eda5c85d694
Leonardo Quixadá <lquixada@gmail.com>
2023-06-01 12:26:23 -0400

    chore: test

--- test/fetch-api/api.spec.ts
+++ test/fetch-api/api.spec.ts
@@ -10,6 +10,7 @@ declare var expect: Chai.ExpectStatic;
 function addFetchSuite () {
   describe('fetch', () => {
     it('should be defined', () => {
+      console.log('test')
       expect(fetch).to.be.a('function')
     })
 
